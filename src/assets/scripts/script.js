d625a25f79ca39ac265520c8e1dec5e6ab047e41
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2022-08-11 15:10:20 +1200

    did mobile stuff

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,12 +1,10 @@
-
-
 // toggle 'dropdown-visible' class for dropdown content
 function myFunction(a) {
     a.parentNode.getElementsByClassName("dropdown-submenu")[0].classList.toggle("dropdown-visible");
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-button')) {
+    if (!event.target.matches('.dropdown-button')) { 
       var dropdowns = document.getElementsByClassName("dropdown-submenu");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -17,3 +15,7 @@ window.onclick = function(event) {
       }
     }
   }
+
+// mobile menu toggle
+// when button clicked, add class 'menu-toggle-open'
+// add class 'mobile-menu-open' to primary-nav
