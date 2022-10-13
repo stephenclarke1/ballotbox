3230e926beaf4e8bdeb3831450e205a4ae14e10d
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2022-10-13 15:43:10 +1300

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
     // eleventyConfig.addPassthroughCopy('./src/assets/styles/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin')
 
     return {
         dir: {
