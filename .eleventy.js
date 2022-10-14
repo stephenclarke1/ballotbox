78d5b9dfe779c46a386e46782e8a5b9e56e5c1ac
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2022-10-14 16:12:04 +1300

    loops in posts section and latest news videos on index

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
+const { DateTime } = require("luxon");
+
+
 module.exports = function(eleventyConfig) {
 
     // eleventyConfig.addPassthroughCopy('./src/assets/styles/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/admin')
+    eleventyConfig.addPassthroughCopy('./src/admin');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
 
     return {
         dir: {
