8804d020ad5411ebb3c7406bbb17d9355b451dd0
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2022-10-15 17:10:05 +1300

    started to add calendar

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
     // eleventyConfig.addPassthroughCopy('./src/assets/styles/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/events');
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
