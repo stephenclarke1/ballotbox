3f84639efb331fa00667000aee64fc3ae8829dd1
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2023-10-28 10:43:16 +1300

    lets see if these filters work

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,23 @@ module.exports = function(eleventyConfig) {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
 
+    //titlecase filter
+    eleventyConfig.addFilter("titlecase", (str) => {
+        return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    });
+
+
+     // Create the filter function.
+    function sortByOrder(values) {
+        let vals = [...values]
+        return vals.sort((a, b) => Math.sign(a.data.order - b.data.order))
+    }
+
+    // Add the filter.
+    eleventyConfig.addFilter('sortByOrder', sortByOrder)
+
     return {
         dir: {
             input: "src",
